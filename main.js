9a74dc8a08d03e675edab825cab0629e372b9a56
idoo <ridhomuhammadghadir@gmail.com>
2025-07-09 22:36:41 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js')
+    .then(() => {
+      console.log('Service Worker terdaftar!');
+    })
+    .catch((error) => {
+      console.log('Gagal daftar SW:', error);
+    });
+}
+
+
+
