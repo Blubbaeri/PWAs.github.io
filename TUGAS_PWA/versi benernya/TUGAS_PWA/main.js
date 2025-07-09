a4df7ccfc30f3410e03a3274030543b194509be9
idoo <ridhomuhammadghadir@gmail.com>
2025-07-09 22:17:01 +0700

    Delete TUGAS_PWA directory
    ulang

--- TUGAS_PWA/versi benernya/TUGAS_PWA/main.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js')
-    .then(() => {
-      console.log('Service Worker terdaftar!');
-    })
-    .catch((error) => {
-      console.log('Gagal daftar SW:', error);
-    });
-}
